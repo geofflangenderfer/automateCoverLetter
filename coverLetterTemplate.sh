508736cf7830dcd3ed4b67ccacd3c647cbf1f8bb
geoff langenderfer <geoff.langenderfer@gmail.com>
2021-04-05 16:34:47 -0500

    changed cl wording

--- coverLetterTemplate.sh
+++ coverLetterTemplate.sh
@@ -15,7 +15,7 @@ I enjoy the problem solving process involved in automating manual processes. For
 
 Most of my personal projects come from a problem I want to solve. And I grow my coding abilities to match. I start from the desired result (I don't like clicking in pgAdmin), then break down the problem into manageable chunks (how can I load students' .sql files?). The problem -> tool approach helps me avoid building something no one wants. For example, I used bash and psql because they eliminated required pgAdmin clicks. 
 
-I am a hard worker. My peers have made me aware of this. And, I enjoy learning from difficult problems.
+I am a hard worker. My peers have acknowledged this. And, I enjoy learning from difficult problems.
 
 I'm passionate about automating painful, manual processes. Working at $@ would allow me to scale that passion to help the most people.
 
@@ -31,7 +31,7 @@ EOF
 name=$(echo $@ | tr " " "_")
 echo -e "$cl"   > tmp.txt
 xclip -sel clip < tmp.txt   && echo "copied to clipboard"
-pandoc -o "$name"_cl.pdf tmp.txt && rm tmp.txt
-mv "$name"_cl.pdf ~/
+#pandoc -o "$name"_cl.pdf tmp.txt && rm tmp.txt
+#mv "$name"_cl.pdf ~/
 
 
